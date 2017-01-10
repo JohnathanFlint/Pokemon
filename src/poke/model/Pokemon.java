9a946b879fc60a56e98e6d96926e8e607737c4a4
JohnathanFlint <nicholas.white142@gmail.com>
2017-01-10 08:48:47 -0700

    Added genters and setters to pokemon abstract class.

--- src/poke/model/Pokemon.java
+++ src/poke/model/Pokemon.java
@@ -13,4 +13,77 @@ public abstract class Pokemon
 		this.name = name;
 		this.number = number;
 	}
+	
+	public String getPokemonTypes()
+	{
+		String pokemonTypes = "This pokemon has the following types:";
+		Class<?> [] types = getClass().getInterfaces();
+		String [] pokeTypes = new String[types.length];
+		
+		for(int index = 0; index < types.length; index++)
+		{
+			String temp = types[index].getCanonicalName();
+			
+			pokeTypes[index] = temp;
+		}
+		
+		for(String current : pokeTypes)
+		{
+			String temp = current.replace(this.getClass().getPackage().getName() + ".", "");
+			
+			pokemonTypes += temp + "\n";
+		}
+		
+		return pokemonTypes;
+	}
+
+	public int getHealthPoints()
+	{
+		return healthPoints;
+	}
+
+	public void setHealthPoints(int healthPoints)
+	{
+		this.healthPoints = healthPoints;
+	}
+
+	public int getAttackPoints()
+	{
+		return attackPoints;
+	}
+
+	public void setAttackPoints(int attackPoints)
+	{
+		this.attackPoints = attackPoints;
+	}
+
+	public int getNumber()
+	{
+		return number;
+	}
+
+	public void setNumber(int number)
+	{
+		this.number = number;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public double getSpeed()
+	{
+		return speed;
+	}
+
+	public void setSpeed(double speed)
+	{
+		this.speed = speed;
+	}
 }
